24aefd2e41c69f916ae7b3e0b985d0d64bd0b4f6
Marco <marcosignorile@Air-di-Marco.homenet.telecomitalia.it>
2023-06-27 20:24:00 +0200

    Creazione rotte category e index_category

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +19,10 @@ Route::get('/home', function () {
 });
 
 
+Route::get('/category', [CategoryController::class, 'category_index'])->name('category_index');
+Route::get('/category/crea', [CategoryController::class, 'category_create'])->name('category_create');
+Route::post('/category/salva', [CategoryController::class, 'category_store'])->name('category_store');
+Route::get('/category/{category}/dettagli', [CategoryController::class, 'category_show'])->name('category_show');
+Route::get('/category/{category}/modifica', [CategoryController::class, 'category_edit'])->name('category_edit');
+Route::put('/category/{category}', [CategoryController::class, 'category_update'])->name('category_update');
+Route::delete('/category/{category}', [CategoryController::class, 'category_destroy'])->name('category_destroy');
