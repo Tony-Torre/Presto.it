0719fe08c718cf314f3ca53ea0075715a6843608
Marco <marcosignorile@Air-di-Marco.homenet.telecomitalia.it>
2023-06-27 19:48:38 +0200

    Icon

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Presto.it</title>
+    <link rel="icon" href="favicon.ico" >
 
     @vite(['resources\css\app.css', 'resources\js\app.js'])
 </head>
