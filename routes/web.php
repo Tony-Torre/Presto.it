d485435c4a5182363652f0cb6a31d02bb74bf4c7
Tony <torretony94@gmail.com>
2023-06-27 19:56:40 +0200

    Aggiunti article mcr

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/home', function () {
     return view('home');
 });
 
-Route::get('/ciao', function () {
-    return view('ciao');
-});
+
