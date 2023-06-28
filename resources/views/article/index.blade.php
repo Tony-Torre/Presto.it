4cea08579de294b08f636b34be08fee2aaf2fe46
Marco <marcosignorile@Air-di-Marco.homenet.telecomitalia.it>
2023-06-28 19:53:05 +0200

    Serch bar

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -1,5 +1,19 @@
 <x-main>
     <h1>I nostri articoli</h1>
+    <form action="{{route('article_serch')}}" method="POST">
+        @method('POST')
+        @csrf
+        <div class="row">
+            <div class="col-md-auto autocomplete">
+                <input type="search" placeholder="Cerca libro" id="article_serch" name="article_serch">
+            </div>
+            <div class="col-md-auto">
+                <div class="d-grid gap-3">
+                    <button class="btn btn-primary btn-lg p-2" type="submit" style='width: 100px; height: 40px;'>Ricerca</button>
+                </div>
+            </div>
+        </div>
+    </form>
     <ul>
     @foreach ($articles as $article)
         <li><span>{{$article['title']}} <a href="{{route('article.edit',['article'=>$article])}}"><button>Modifica</button></a> <a href="{{route('article.show',['article'=>$article])}}"><button>Visualizzalo</button></a></span></li>
