745d08ee46abec334d43dfa3ba1daf7e262b4862
Marco <marcosignorile@Air-di-Marco.homenet.telecomitalia.it>
2023-06-29 21:26:11 +0200

    new cursor

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -1,7 +1,7 @@
 <x-main>
     <h1 class="text-center">I nostri articoli</h1>
     <ul>
-    <div class="row row-cols-1 row-cols-md-3 g-4 w-75 m-auto " >
+    <div class="row row-cols-1 row-cols-md-3 g-4 w-75 m-auto" >
     @foreach ($articles as $article)
         <x-card_article :article="$article" />
     @endforeach
