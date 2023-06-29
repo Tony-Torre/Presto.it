99d6fb42fc4807201d8c06b348ef27b27284fac8
Gianclaudio <gianclaudiotarantini@gmail.com>
2023-06-29 18:56:34 +0200

    nulla

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -1,12 +1,28 @@
 <span>{{$article['title']}}</span>
 
-<div class="card" style="width: 18rem;">
-    <img class="card-img-top mb-5 mb-md-0"
-                        src="{{empty($book->image) ? Storage::url('/images/default.png') : Storage::url($book->image)}}"
-                        alt="...">
-    <div class="card-body">
-      <h5 class="card-title">{{$article['title']}}</h5>
-      <p class="card-text">{{$article['description']}}</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+<div id="carouselExampleIndicators" class="carousel slide">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="..." class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="..." class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="..." class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
   </div>
