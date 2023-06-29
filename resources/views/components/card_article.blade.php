79f0f6cd5ba88aee34ded1c269508bd1a3fcf072
Tony <torretony94@gmail.com>
2023-06-30 01:08:26 +0200

    Sistemata grafica index

--- resources/views/components/card_article.blade.php
+++ resources/views/components/card_article.blade.php
@@ -1,14 +1,17 @@
 <div class="col">
-    <div class="card">
-      <img src="https://unsplash.it/400" class="card-img-top" alt="immagine">
-      <div class="card-body">
-        <h5 class="card-title">{{$article['title']}}</h5>
-        <p class="card-text">Descrizione:{{$article['description']}}</p>
-        <p class="card-text">Categoria:{{$article->category->name}}</p>
-        @auth
-          <a href="{{route('article.edit',['article'=>$article])}}"><button>Modifica</button></a>
-        @endauth 
-        <a href="{{route('article.show',['article'=>$article])}}"><button>Dettagli</button></a>
+  <div class="card background_white">
+    <img src="https://unsplash.it/400" class="card-img-top " alt="immagine">
+    <div class="card-body ">
+      <h5 class="card-title">{{$article['title']}}</h5>
+      <p class="card-text">{{$article['description']}}</p>
+      <div class="d-flex justify-content-between mb-2">
+        <span class="background_blue rounded p-1 ">{{$article->category->name}}</span>
+        <span style="color: rgb(0, 167, 0)" class="rounded p-1 ">€{{$article->price}}</span>
       </div>
+      @auth
+        <a href="{{route('article.edit',['article'=>$article])}}"><button class="btn btn-light">Modifica</button></a>
+      @endauth 
+      <a href="{{route('article.show',['article'=>$article])}}"><button class="btn btn-light">Dettagli</button></a>
     </div>
   </div>
+</div>
