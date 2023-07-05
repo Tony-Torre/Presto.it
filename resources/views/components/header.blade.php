ff742a7c3de6f4321a003f3deab3abbf664279c5
Gianclaudio <gianclaudiotarantini@gmail.com>
2023-07-05 19:57:13 +0200

    nav-bar con revisor

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,7 +2,7 @@
     <div class="container background_white">
         <div class="row">
             <div class="col-xl-8 col-lg-12 p-0">
-                <img src="\img\ecommerce.jpg" alt="" class="img-fluid">
+                <img src="\img\ecommerce.jpg" alt="" class="img-fluid img-header">
             </div>
             <div class="d-flex flex-column justify-content-center text-center col-xl-4 col-lg-12 p-0 background_blue">
                 <span style="font-size: 35px" class="mt-5">
