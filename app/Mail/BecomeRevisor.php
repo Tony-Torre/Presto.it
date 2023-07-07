ed4eceea439c921b73f6de14d3b8288137844cd7
Gianclaudio <gianclaudiotarantini@gmail.com>
2023-07-07 21:49:22 +0200

    sistemato form revisore

--- app/Mail/BecomeRevisor.php
+++ app/Mail/BecomeRevisor.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
@@ -34,7 +35,8 @@ class BecomeRevisor extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'Become Revisor',
+            from: new Address('Ciao@noreplay.com', 'Presto.it'),
+            subject: 'Richiesta inviata',
         );
     }
 
