5457899669d04951ea6a2c8e64c309f867d58852
Gianclaudio <gianclaudiotarantini@gmail.com>
2023-07-10 21:44:45 +0200

    footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,19 +1,33 @@
-<div class="footer mt-5">&copy;
-    <span id="year">
-        @auth
-        @if (Auth::user()->is_revisor)
-        Lavori già per noi
-        @else
-        <a href="{{route('revisor.form')}}">{{__('ui.revisorfoot')}}</a>
-        @endif
-        @else
-        <a href="{{route('revisor.form')}}">{{__('ui.revisorfoot')}}</a>
-        @endauth
-        
-    </span>
-    <ul>
-        <li><x-_locale lang="it" nation='it' /></li>
-        <li><x-_locale lang="es" nation='es' /></li>
-        <li><x-_locale lang="en" nation='gb' /></li>
-    </ul>
+<div class="footer mt-5 ">&copy;
+    <div class="row">
+        <div class="col">
+            <span id="year">
+                @auth
+                    @if (Auth::user()->is_revisor)
+                        Lavori già per noi
+                    @else
+                        <a href="{{ route('revisor.form') }}">{{ __('ui.revisorfoot') }}</a>
+                    @endif
+                @else
+                    <a href="{{ route('revisor.form') }}">{{ __('ui.revisorfoot') }}</a>
+                @endauth
+            </span>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12 " >
+            <ul class="horizontal-list m-2">
+                <li>
+                    <x-_locale lang="it" nation='it' />
+                </li>
+                <li>
+                    <x-_locale lang="es" nation='es' />
+                </li>
+                <li>
+                    <x-_locale lang="en" nation='gb' />
+                </li>
+            </ul>
+        </div>
+    </div>
 </div>
