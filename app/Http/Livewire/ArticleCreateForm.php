73a499dbac0d90b6512192bc62fde185c17e2d58
Gianclaudio <gianclaudiotarantini@gmail.com>
2023-07-11 20:40:12 +0200

    user06 metà

--- app/Http/Livewire/ArticleCreateForm.php
+++ app/Http/Livewire/ArticleCreateForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Jobs\ResizeImage;
 use App\Models\Article;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
@@ -16,36 +17,40 @@ class ArticleCreateForm extends Component
     public $temporary_images;
 
     protected $rules = [
-        'title'=> 'required|string|max:225|min:5',
-        'price'=> 'required|decimal:2',
-        'description'=> 'required|string|max:225|min:5',
-        'category_id'=> 'integer',
-        'user_id'=> '',
-        'temporary_images.*'=> 'image|max:3072',
+        'title' => 'required|string|max:225|min:5',
+        'price' => 'required|decimal:2',
+        'description' => 'required|string|max:225|min:5',
+        'category_id' => 'integer',
+        'user_id' => '',
+        'temporary_images.*' => 'image|max:3072',
         'images.*' => 'image|max:3072',
     ];
-    
-    public function updated($propertyName){
+
+    public function updated($propertyName)
+    {
         $this->validateOnly($propertyName);
     }
-    
-    public function updatedTemporaryImages(){
-        if($this->validate([
-            'temporary_images.*'=> 'image|max:3072',
+
+    public function updatedTemporaryImages()
+    {
+        if ($this->validate([
+            'temporary_images.*' => 'image|max:3072',
         ])) {
-            foreach ($this->temporary_images as $image){
-            $this->images[] = $image;
+            foreach ($this->temporary_images as $image) {
+                $this->images[] = $image;
             }
         }
     }
 
-    public function removeImages($key) {
+    public function removeImages($key)
+    {
         if (in_array($key, array_keys($this->images))) {
             unset($this->images[$key]);
         }
     }
 
-    public function store(){
+    public function store()
+    {
         $this->validate();
         $article = Article::create([
             'title' => $this->title,
@@ -54,17 +59,25 @@ class ArticleCreateForm extends Component
             'category_id' => $this->category_id,
             'user_id' => Auth::user()->id,
         ]);
-        if(count($this->images)) {
-            foreach($this->images as $image) {
-                $article->images()->create(['path'=>$image->store('image', 'public')]);
+        if (count($this->images)) {
+            foreach ($this->images as $image) {
+                // $article->images()->create(['path' => $image->store('image', 'public')]);
+
+
+                $this->reset(['title', 'price', 'description', 'category_id', 'images', 'temporary_images']);
+                $newFileName = "articles/{$this->article->id}";
+                $newImage = $this->articles->images()->create(['path' => $image->store('$newFileName')]);
+
+                dispatch(new ResizeImage($newImage->path , 400 , 300));
+                session()->flash('article', 'Articolo aggiunto correttamente');
             }
+
+            File::deleteDirectory(storage_path('/app/livewire-tmp'));
         }
-        $this->reset(['title','price','description','category_id','images','temporary_images']);
-        session()->flash('article', 'Articolo aggiunto correttamente');
-       
     }
-    
-    public function messages(){
+
+    public function messages()
+    {
         return [
             'title.max' => 'Inserisci massimo 255 caratteri',
             'title.min' => 'Inserisci almeno 5 caratteri',
@@ -75,9 +88,9 @@ class ArticleCreateForm extends Component
             'description.max' => 'Inserisci massimo 255 caratteri',
             'description.required' => 'Descizione obbligatoria',
             'description.string' => 'La descrizione deve essere composta di lettere',
-            'category_id.integer'=>'Seleziona una categoria',
-            'temporary_images.image'=>'Il file deve essere un\'immagine',
-            'temporary_images.max'=>'L\'immagine deve essere al massimo di 3 mb'
+            'category_id.integer' => 'Seleziona una categoria',
+            'temporary_images.image' => 'Il file deve essere un\'immagine',
+            'temporary_images.max' => 'L\'immagine deve essere al massimo di 3 mb'
         ];
     }
     public function render()
