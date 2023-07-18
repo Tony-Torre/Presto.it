0190d5be6583c1dbf9ceb428d138319bed0765bc
Tony <torretony94@gmail.com>
2023-07-18 20:47:22 +0200

    Aggiunto footer/ sistemata grafica mail

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,33 +1,55 @@
-<div class="footer mt-5">&copy;
-    <div class="row">
-        <div class="col">
-            <span id="year">
-                @auth
-                    @if (Auth::user()->is_revisor)
-                        {{__('ui.Lavori_gia')}}
-                    @else
-                        <a href="{{ route('revisor.form') }}">{{ __('ui.revisorfoot') }}</a>
-                    @endif
-                @else
-                    <a href="{{ route('revisor.form') }}">{{ __('ui.revisorfoot') }}</a>
-                @endauth
-            </span>
-        </div>
+<footer class="footer mt-5">
+    <div class="container">
+        <div class="row mb-2">
+            <div class="col-3">
+                <h5 class="text-center mt-2">Presto.it</h5>
+                <ul class="list-group list-group-flush text-center">
+                    <li style="list-style-type: none">
+                        @auth
+                        @if (Auth::user()->is_revisor)
+                        <a href="{{route('revisor.list')}}">Revisiona articoli</a>
+                        @else
+                        <a href="{{ route('revisor.form') }}">Lavora con noi</a>
+                        @endif
+                        @else
+                        <a href="{{ route('revisor.form') }}">Lavora con noi</a>
+                        @endauth
+                    </li>
+                    <li style="list-style-type: none"><a href="{{route('article.index')}}">Guarda gli annunci</a></li>
+                    <li style="list-style-type: none"><a href="{{route('category.index')}}">Guarda le categorie</a></li>
+                </ul>
+            </div>
+            <div class="col-3">
+                <h5 class="text-center mt-2">Profilo</h5>
+                <ul class="list-group list-group-flush text-center">
+                    <li style="list-style-type: none"><a href="{{route('article.create')}}">Inserisci annuncio</a></li>
+                    <li style="list-style-type: none"><a href="{{route('user.index')}}">Controlla i tuoi annunci</a></li>
+                    <li style="list-style-type: none"><a href="{{route('user.show',['user'=>Auth::user()])}}">Guarda il tuo profilo</a></li>
+                </ul>
+            </div>
+            <div class="col-3">
+                <h5 class="text-center mt-2">Contattaci</h5>
+                <ul class="list-group list-group-flush text-center">
+                    <li style="list-style-type: none"><a href="https://www.linkedin.com/in/marco-signorile/" target="_blank"><i class="fa-brands fa-linkedin"></i> Signorile Marco</a></li>
+                    <li style="list-style-type: none"><a href="https://www.linkedin.com/in/gianclaudio-tarantini-882325176/" target="_blank"><i class="fa-brands fa-linkedin"></i> Tarantini Gianclaudio</a></li>
+                    <li style="list-style-type: none"><a href="https://www.linkedin.com/in/antonino-torre-dev/" target="_blank"><i class="fa-brands fa-linkedin"></i> Torre Antonino</a></li>
+                </ul>
+            </div>
+            <div class="col-3 " >
+                <h5 class="text-center">Lingue</h5>
+                <ul class="m-2 text-center list-group list-group-flush">
+                    <li style="list-style-type: none">
+                        <x-_locale lang="it" nation='it' />
+                    </li>
+                    <li style="list-style-type: none">
+                        <x-_locale lang="es" nation='es' />
+                    </li>
+                    <li style="list-style-type: none">
+                        <x-_locale lang="en" nation='gb' />
+                    </li>
+                </ul>
+            </div>
+        </div>   
     </div>
-
-    <div class="row">
-        <div class="col-12 " >
-            <ul class="horizontal-list m-2">
-                <li>
-                    <x-_locale lang="it" nation='it' />
-                </li>
-                <li>
-                    <x-_locale lang="es" nation='es' />
-                </li>
-                <li>
-                    <x-_locale lang="en" nation='gb' />
-                </li>
-            </ul>
-        </div>
-    </div>
+    <div class="text-center mt-3 mb-0" style="font-size: small">&copy; Copyright I Moschettieri Powered Aulab 2023</div>
 </div>
