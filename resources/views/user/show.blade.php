1e606f76daa4245fc1150109776b96116ada9863
Tony <torretony94@gmail.com>
2023-07-19 19:51:49 +0200

    Aggiunto img watermarks

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -36,7 +36,7 @@
                             <div class="row g-0">
                                 <div class="col-md-4">
                                     <a href="{{ route('article.show', ['article' => $article]) }}">
-                                        <img src="{{ $article->images()->get()->isEmpty()? '\img\no-image.jpg': $article->images()->first()->getUrl(400, 300) }}"
+                                        <img src="{{ $article->images()->get()->isEmpty()? '\img\no-image.jpg': $article->images()->first()->getUrl(400, 400) }}"
                                         class="img-fluid rounded-start" alt="article-photo">
                                     </a>
                                 </div>
